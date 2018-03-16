71c76a59ea398327885a25269e082f5ad17633d2
Logan Prough <loganprough@ksu.edu>
2018-03-16 14:30:01 -0500

    Use function Mark implemented

--- pintos-anon/src/userprog/syscall.c
+++ pintos-anon/src/userprog/syscall.c
@@ -33,6 +33,7 @@ int sys_write(int fd, char *s, unsigned int size) {
 }
 
 int sys_open(char *filename) {
+  if (filename == NULL) return -1;
   struct fd_struct *fds = palloc_get_page(PAL_ZERO);
   memset(fds, 0, sizeof(*fds));
   fds->file = filesys_open(filename);
@@ -92,25 +93,25 @@ struct fd_struct *fd_item(int fd) {
   return NULL;
 }
 
-int sys_wait(pid_t pid) {
+int sys_wait(tid_t pid) {
 	return process_wait(pid);
 }
 
 // Both of the following two functions are derived from ryantimwilson's work
 // Reference: https://github.com/ryantimwilson/Pintos-Project-2/blob/master/src/userprog/syscall.c :298
 void is_pointer_valid(const void *vaddr) {
-	if (!is_user_vaddr(vaddr) || vaddr < 0x08048000) {
-		exit(-1);
+	if (!is_user_vaddr(vaddr) || vaddr < (void *)0x08048000) {
+		thread_exit(-1);
 	}
 }
 
-int user_kernal_conversion(const void *vaddr)
+int user_kernel_conversion(const void *vaddr)
 {
 	is_pointer_valid(vaddr);
 	void *ptr = pagedir_get_page(thread_current()->pagedir, vaddr);
 	if (!ptr)
 	{
-		exit(-1);
+		thread_exit(-1);
 	}
 	return (int)ptr;
 }
@@ -127,25 +128,23 @@ syscall_handler (struct intr_frame *f UNUSED)
   // find out which syscall and do it
   int *p = f->esp;
   int number = *p;
+	// Call system functions with parameters. Referenced https://github.com/pindexis/pintos-project2/blob/master/userprog/syscall.c to get some of the typecasts to stop complaining
   switch (number) {
   case SYS_WRITE:
-	  // Call sys_write with parameters. Referenced https://github.com/pindexis/pintos-project2/blob/master/userprog/syscall.c to get some of the typecasts to stop complaining
-	  sys_write(*(int *)(f->esp + 4), *(char **)(f->esp + 8), *(unsigned int *)(f->esp + 12)); break;
-  case SYS_WAIT: while (1) {}; break; // TODO actually implement wait
+	  sys_write(*(int *)user_kernel_conversion(f->esp + 4), *(char **)user_kernel_conversion(f->esp + 8), *(unsigned int *)user_kernel_conversion(f->esp + 12)); break;
+  case SYS_WAIT: while (1) {int x = 4; x++; int y = x / 2;}; break; // TODO actually implement wait
   case SYS_HALT: shutdown(); break;
-  case SYS_EXIT: thread_exit(*((int *)f->esp + 1)); break; // TODO make sure pointer valid
-  case SYS_EXEC: retval = process_execute(*(char **)(f->esp + 4)); break; // TODO make sure pointer valid
-  case SYS_CREATE: retval = filesys_create(*(char **)(f->esp + 4), *(int *)(f->esp + 8)); break; // TODO ^^
-  case SYS_REMOVE: retval = filesys_remove(*(char **)(f->esp + 4)); break; // TODO ^^
-  case SYS_OPEN: sys_open(*(char **)(f->esp + 4)); break; // TODO ^^
-  case SYS_CLOSE: sys_close(*(int *)(f->esp + 4)); break; // TODO ^^
-  case SYS_READ: sys_read(*(int *)(f->esp + 4), *(char **)(f->esp + 8), *(unsigned int *)(f->esp + 12)); break; // TODO ^^
-  case SYS_SEEK: sys_seek(*(int *)(f->esp + 4), *(unsigned int *)(f->esp + 8)); break; // TODO ^^
-  case SYS_TELL: sys_tell(*(int *)(f->esp + 4)); break; // TODO ^^
-  case SYS_FILESIZE: sys_filesize(*(int *)(f->esp + 4)); break; // TODO ^^
+  case SYS_EXIT: thread_exit(*(int *)user_kernel_conversion(f->esp + 4)); break;
+  case SYS_EXEC: retval = process_execute(*(char **)user_kernel_conversion(f->esp + 4)); break;
+  case SYS_CREATE: retval = filesys_create(*(char **)user_kernel_conversion(f->esp + 4), *(int *)user_kernel_conversion(f->esp + 8)); break;
+  case SYS_REMOVE: retval = filesys_remove(*(char **)user_kernel_conversion(f->esp + 4)); break;
+  case SYS_OPEN: sys_open(*(char **)user_kernel_conversion(f->esp + 4)); break;
+  case SYS_CLOSE: sys_close(*(int *)user_kernel_conversion(f->esp + 4)); break;
+  case SYS_READ: sys_read(*(int *)user_kernel_conversion(f->esp + 4), *(char **)user_kernel_conversion(f->esp + 8), *(unsigned int *)user_kernel_conversion(f->esp + 12)); break;
+  case SYS_SEEK: sys_seek(*(int *)user_kernel_conversion(f->esp + 4), *(unsigned int *)user_kernel_conversion(f->esp + 8)); break;
+  case SYS_TELL: sys_tell(*(int *)user_kernel_conversion(f->esp + 4)); break;
+  case SYS_FILESIZE: sys_filesize(*(int *)user_kernel_conversion(f->esp + 4)); break;
   default: thread_exit(0);
-
-  
  }
 
   if (retval < 0) thread_exit(retval); // Syscall went wrong
